456be8e62c36c4f1aaf2a78d61841b5d92e55ffb
Hongyu Li <710418161@qq.com>
2014-02-10 19:08:49 -0800

    Edited newPlaceitActivity and layout

--- PlaceItApp/src/edu/ucsd/placeitapp/NewPlaceitActivity.java
+++ PlaceItApp/src/edu/ucsd/placeitapp/NewPlaceitActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class NewPlaceitActivity extends Activity {
 
+	private CheckBox check;
+	private EditText editName,editDes,editTime;
+	private String name,des,time;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_placeit);
+		
+		
 	}
 
 	@Override
@@ -23,4 +30,17 @@ public class NewPlaceitActivity extends Activity {
 	public void back(View view) {
 		finish();
 	}
+	
+	public void createPlaceIt(View view) {
+		check = (CheckBox) findViewById(R.id.checkBox1);
+		editName = (EditText) findViewById(R.id.editName);
+		editDes = (EditText) findViewById(R.id.editDes);
+		editTime = (EditText) findViewById(R.id.editTime);
+		name = editName.getText().toString();
+		des = editDes.getText().toString();
+		time = editTime.getText().toString();
+		//TODO
+		finish();
+	}
+
 }
