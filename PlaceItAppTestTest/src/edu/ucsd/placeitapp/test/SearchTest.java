016699d0fa6d2674ed88e9c37b619cdb5bf5bc92
EunChul <dikenande@gmail.com>
2014-02-16 22:42:43 -0800

    a

--- PlaceItAppTestTest/src/edu/ucsd/placeitapp/test/SearchTest.java
+++ PlaceItAppTestTest/src/edu/ucsd/placeitapp/test/SearchTest.java
@@ -1,31 +1,57 @@
 package edu.ucsd.placeitapp.test;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.robotium.solo.Solo;
+import edu.ucsd.placeitapp.R;
+import android.app.Activity;
 import android.location.Location;
-import android.test.AndroidTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.RenamingDelegatingContext;
+import android.widget.Button;
+import android.widget.EditText;
 import edu.ucsd.placeitapp.*;
 
-public class SearchTest extends AndroidTestCase {
-
-	public void setUp() {
-		PlaceItDBHelper.setInstance(new RenamingDelegatingContext(getContext(),
-				"test_"));
-		PlaceItList.setInstance(new RenamingDelegatingContext(getContext(),
-				"test_"));
-
+public class SearchTest extends ActivityInstrumentationTestCase2<MapActivity> {
+	
+	public SearchTest() {
+		super(MapActivity.class);	
 	}
 
-	public void tearDown() {
+	private Solo solo;
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		PlaceItDBHelper.setInstance(new RenamingDelegatingContext(getActivity().getBaseContext(),
+				"test_"));
+		PlaceItList.setInstance(new RenamingDelegatingContext(getActivity().getBaseContext(),
+				"test_"));
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+
+	}
+	@Override
+	public void tearDown() throws Exception {
 		PlaceItDBHelper.getInstance().close();
+		solo.finishOpenedActivities();
+		super.tearDown();
 	}
 	
-	private void searchFor(String location) {}
+	private void searchFor(String location) {
+		EditText searchBox = (EditText) solo.getView(R.id.SearchBar);
+		solo.enterText(searchBox, location);		
+		solo.clickOnButton("Find");
+	}
 	
-	private void mapCameraAt(Location location) {
+	private void mapCameraAt(LatLng location) {
 		
 	}
 	
-	public void testSearchAddress() {}
+	public void testSearchAddress() {
+        solo.waitForActivity("MapActivity");
+        solo.sleep(5000);
+		searchFor("UCSD");
+	}
 	
 	public void testSearchName() {}
 
