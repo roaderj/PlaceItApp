91df3cb01020420a42a9a79ea5ff218b82c18968
EunChul <dikenande@gmail.com>
2014-02-23 17:47:41 -0800

    Previously MainActivity has became MainMenuActivity. MainActivity will be used as log-in page

--- PlaceItApp/src/edu/ucsd/placeitapp/MainMenuActivity.java
+++ PlaceItApp/src/edu/ucsd/placeitapp/MainMenuActivity.java
@@ -7,17 +7,18 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainMenuActivity extends Activity {
 	public final static String PLACEIT_ID = "edu.ucsd.placeitapp.PLACEIT_ID";
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_main);
 
 		PlaceItDBHelper.setInstance(this.getApplicationContext());
-		PlaceItList.setInstance(this.getApplicationContext());
+		PlaceItList.setInstance(this.getApplicationContext()); 
+		
 	}
 
 	@Override
@@ -25,6 +26,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void goToMap(View view) {
+		Intent intent = new Intent(this, MapActivity.class);
+		startActivity(intent);
+	}
+
+	public void goToList(View view) {
+		Intent intent = new Intent(this, ListViewActivity.class);
+		startActivity(intent);
 	}
 
 }
