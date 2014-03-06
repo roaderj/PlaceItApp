e2d81583a934fdf5474b4cab601b59fdda3f552a
Madhu Krishnan <MadhuV.Krishnan@gmail.com>
2014-03-05 17:19:12 -0800

    Added CategoricalPlaceIt class for someone to modify.

---
+++ PlaceItApp/src/edu/ucsd/placeitapp/CategoricalPlaceIt.java
@@ -0,0 +1,18 @@
+package edu.ucsd.placeitapp;
+
+public class CategoricalPlaceIt { //extends PlaceIt {
+	
+	public CategoricalPlaceIt(String title, String description, String[] tags) {
+		//TODO
+	}
+	
+	public void trackLocation() {
+		//TODO 
+	}
+	
+	public String toString() {
+		//TODO
+		return super.toString(); //temporary
+	}
+
+}
