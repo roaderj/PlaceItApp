7cc7057d5239374f99e2a95a350c8144d20f1139
Ho-Wei Kang <howeik@gmail.com>
2014-03-15 22:32:34 -0700

    added MapTest and SearchTest

---
+++ PlaceItAppTestTest/src/edu/ucsd/placeitapp/test/MapTest.java
@@ -0,0 +1,30 @@
+package edu.ucsd.placeitapp.test;
+
+import com.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.RenamingDelegatingContext;
+import edu.ucsd.placeitapp.*;
+import edu.ucsd.placeitapp.model.PlaceItList;
+
+public class MapTest {
+	private Solo solo;
+	public MapTest() {
+	}
+
+	public void setUp() {
+	}	
+
+	public void tearDown() {
+	}
+	
+	// Given the user open the app
+	// When the user click on the map button
+	// Then the map show
+	public void testOpenMap() {
+//		solo.assertCurrentActivity("wrong activity", MainActivity.class);
+//		solo.clickOnButton("Map");
+//		solo.assertCurrentActivity("wrong activity", MapActivity.class);
+	}
+	
+}
---
+++ PlaceItAppTestTest/src/edu/ucsd/placeitapp/test/SearchTest.java
@@ -0,0 +1,43 @@
+package edu.ucsd.placeitapp.test;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.robotium.solo.Solo;
+
+import edu.ucsd.placeitapp.R;
+import android.app.Activity;
+import android.location.Location;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.RenamingDelegatingContext;
+import android.widget.Button;
+import android.widget.EditText;
+import edu.ucsd.placeitapp.*;
+import edu.ucsd.placeitapp.model.EntityDb;
+import edu.ucsd.placeitapp.model.PlaceItList;
+
+public class SearchTest {
+	
+	public SearchTest() {
+	}
+
+	private Solo solo;
+	
+	public void setUp() {
+	}
+
+	
+	public void tearDown() {
+	}
+	
+	private void searchFor(String location) {
+	}
+	
+	private void mapCameraAt(LatLng location) {
+		
+	}
+	
+	public void testSearchAddress() {
+	}
+	
+	public void testSearchName() {}
+
+}
