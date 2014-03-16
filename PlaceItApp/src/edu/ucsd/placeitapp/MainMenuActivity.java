e03d941c8ca4e0de356e5a8844299b39d0bc994c
Hongyu Li <710418161@qq.com>
2014-03-15 17:57:38 -0700

    edited logout

--- PlaceItApp/src/edu/ucsd/placeitapp/MainMenuActivity.java
+++ PlaceItApp/src/edu/ucsd/placeitapp/MainMenuActivity.java
@@ -1,5 +1,9 @@
 package edu.ucsd.placeitapp;
 
+import java.util.List;
+
+import edu.ucsd.placeitapp.model.PlaceIt;
+import edu.ucsd.placeitapp.model.PlaceItDb;
 import edu.ucsd.placeitapp.model.PlaceItList;
 import android.os.Bundle;
 import android.app.Activity;
@@ -56,6 +60,18 @@ public class MainMenuActivity extends Activity {
 	 * Logout button click action
 	 */
 	public void logOut(View v) {
+		//TODO: push all changes to the server
+		PlaceItDb instance = PlaceItDb.getInstance();
+		List<PlaceIt> placeIts = PlaceItList.all(); 
+		for (PlaceIt placeIt : placeIts) {
+			//disable all trackLocations
+			placeIt.trackLocation(this, false);
+			//disable all alarms
+			placeIt.setAlarm(this, false);
+			//drop the current table
+			instance.delete(placeIt);
+		}
+		//disable the current user
 		SyncClient.logOut(); 
 		//return to login window
 		Intent returnSignUp = new Intent(this, MainActivity.class); 
